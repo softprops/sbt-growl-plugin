5b978b9ac821e3ae0bd2743810cb0a5c8bb42998
doug tangren <d.tangren@gmail.com>
2010-03-09 21:28:26 -0500

    updating to sbt 0.7.1

--- project/build/GrowlPluginProject.scala
+++ project/build/GrowlPluginProject.scala
@@ -1,8 +1,7 @@
 import sbt._
 
 class GrowlPluginProject(info: ProjectInfo) extends PluginProject(info) {
-	override def crossScalaVersions = Set("2.7.3", "2.7.4", "2.7.5", "2.7.6", "2.7.7") 
-  val stSnapshots = "Scala Tools Snapshots" at "http://www.scala-tools.org/repo-snapshots/"
+	val stSnapshots = "Scala Tools Snapshots" at "http://www.scala-tools.org/repo-snapshots/"
   val specs = "org.scala-tools.testing" % "specs" % "1.6.2-SNAPSHOT" % "test"
   
   val lessRepo = "lessis repo" at "http://repo.lessis.me"
