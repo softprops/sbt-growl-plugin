59e1396b511a6661e05c82241194b8710fdcdfb2
doug tangren <d.tangren@gmail.com>
2010-06-01 22:34:03 -0700

    meow bump

--- project/build/GrowlPluginProject.scala
+++ project/build/GrowlPluginProject.scala
@@ -5,7 +5,7 @@ class GrowlPluginProject(info: ProjectInfo) extends PluginProject(info) with pos
   val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1-SNAPSHOT" % "test"
   
   val lessRepo = "lessis repo" at "http://repo.lessis.me"
-  val meow = "me.lessis" %% "meow" % "0.0.2"
+  val meow = "me.lessis" %% "meow" % "0.1.0"
   
   override def managedStyle = ManagedStyle.Maven
   lazy val publishTo = Resolver.file("publish repo", new java.io.File("/var/www/repo"))
